e9cb34c2c6e44aa37b2a5244b33160a1faadf680
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2021-01-16 02:54:00 -0500

    bug fix

--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -7,8 +7,8 @@ const LoginForm = () => {
   const dispatch = useDispatch();
   const auth = useSelector(state => state.auth);
   const [formData, setFormData] = useState({
-    email: 'test@test.com',
-    password: 'password'
+    email: '',
+    password: ''
   });
   const [inputType, setType] = useState('password');
   const { email, password } = formData;
